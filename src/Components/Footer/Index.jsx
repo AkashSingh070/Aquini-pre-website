c0efa5ee2d63a64bb414ed18fd6e134bb9e63182
singhAkash790 <113873172+singhAkash790@users.noreply.github.com>
2025-02-24 15:12:23 +0530

    adding the data

--- src/Components/Footer/Index.jsx
+++ src/Components/Footer/Index.jsx
@@ -85,7 +85,7 @@ const Index = ({ isActive, isMobile }) => {
           </div>
 
           {/* Top Footer Section */}
-          <div className="flex flex-wrap justify-center md:justify-between border-y border-white/10 py-9 my-9 xl:py-24 md:py-12 md:my-10">
+          <div className="flex flex-wrap justify-center md:justify-between border-y border-white/10 py-7 my-7 xl:py-24 md:py-12 md:my-10">
             {/* Contact Info */}
             <div className="w-1/2 xl:w-1/6  mb-8 xl:mb-0 text-center md:text-left">
               <ul className="space-y-3 mx-auto w-fit">
@@ -121,7 +121,7 @@ const Index = ({ isActive, isMobile }) => {
             </div>
 
             {/* Email */}
-            <div className="w-1/2 xl:w-1/6 mb-8 xl:mb-0 text-center md:text-left">
+            <div className="w-1/2 xl:w-1/6  xl:mb-0 text-center md:text-left">
               <ul className="space-y-3 mx-auto w-fit">
                 <li className="primary-color font-semibold text-xl mb-4">
                   <ContactIcon icon={FaEnvelope} />
@@ -140,7 +140,7 @@ const Index = ({ isActive, isMobile }) => {
             </div>
 
             {/* Social Media */}
-            <div className="w-1/2 xl:w-1/6 mb-8  xl:mb-0 text-center md:text-left">
+            <div className="w-1/2 xl:w-1/6   xl:mb-0 text-center md:text-left">
               <ul className="space-y-3 mx-auto w-fit">
                 <li className="primary-color font-semibold text-xl mb-4">
                   Follow on
